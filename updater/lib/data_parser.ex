175e01f3c8eb8ad1a433c54c3ec7196670296881
Roman Heinrich <roman.heinrich@gmail.com>
2018-07-13 12:57:52 +0200

    tests for data parser

--- updater/lib/data_parser.ex
+++ updater/lib/data_parser.ex
@@ -9,9 +9,19 @@ defmodule Updater.DataParser do
     |> Enum.map(&parse_line/1)
   end
 
-  defp parse_line(line) do
-    [url, tags_string] = line |> String.split(" [")
-    tags = tags_string |> String.replace("]", "") |> String.replace(" ", "") |> String.split(",")
-    {url, tags}
+  def parse_line(line) do
+    cond do
+      String.contains?(line, "[") -> parse_line_with_tags(line)
+      true -> {line, [], ""}
+    end
+  end
+
+  def parse_line_with_tags(line) do
+    [url, tags_and_description] = line |> String.split(" [")
+
+    [tags_string, description]  = tags_and_description |> String.split("]",  parts: 2)
+    tags = tags_string |> String.replace("]", "") |> String.replace(" ", "") |> String.split(",") |> Enum.reject(fn(x)-> x == "" end)
+    description = description |> String.trim()
+    {url, tags, description}
   end
 end
---
+++ updater/test/data_parser_test.exs
@@ -0,0 +1,26 @@
+defmodule Updater.DataParserTest do
+  use ExUnit.Case
+  alias Updater.DataParser
+
+  describe ".parse_line" do
+    test "without tags" do
+      res = DataParser.parse_line("https://github.com/boilercoding/crm [] Contact Management made with Elixir")
+      assert res == {"https://github.com/boilercoding/crm", [], "Contact Management made with Elixir"}
+    end
+
+    test "without tags / description" do
+      res = DataParser.parse_line("https://github.com/boilercoding/crm")
+      assert res == {"https://github.com/boilercoding/crm", [], ""}
+    end
+
+    test "without tags, but description" do
+      res = DataParser.parse_line("https://github.com/boilercoding/crm [] this is nice")
+      assert res == {"https://github.com/boilercoding/crm", [], "this is nice"}
+    end
+
+    test "with tags and no description" do
+      res = DataParser.parse_line("https://github.com/boilercoding/crm [crm, saas, nice stuff]")
+      assert res == {"https://github.com/boilercoding/crm", ["crm", "saas", "nicestuff"], ""}
+    end
+  end
+end
