07d1baee0e51ea17857eb709bf27d36fc89c3104
Roman Heinrich <roman.heinrich@gmail.com>
2018-07-24 13:16:51 +0200

    fix querier

--- updater/lib/querier.ex
+++ updater/lib/querier.ex
@@ -1,14 +1,14 @@
 defmodule Updater.Querier do
   def for_tag(tag) do
     data()
-    |> Enum.filter(fn {_url, tags} ->
+    |> Enum.filter(fn {_url, tags, _desc} ->
       Enum.any?(tags, fn x -> x |> String.contains?(tag) end)
     end)
   end
 
   def tags do
     data()
-    |> Enum.reduce([], fn {_url, tags}, acc -> (acc ++ tags) |> Enum.uniq() end)
+    |> Enum.reduce([], fn {_url, tags, _desc}, acc -> (acc ++ tags) |> Enum.uniq() end)
     |> Enum.sort()
   end
 
