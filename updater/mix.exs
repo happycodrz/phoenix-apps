328aa30fec5abcee063d9a9cec967c97cf3b3fba
Roman Heinrich <roman.heinrich@gmail.com>
2018-07-28 18:18:08 +0200

    use flexi

--- updater/mix.exs
+++ updater/mix.exs
@@ -22,6 +22,7 @@ defmodule Updater.MixProject do
   defp deps do
     [
       {:cortex, "~> 0.5"},
+      {:flexi, "~> 0.3"},
       {:floki, "~> 0.20.0"},
       {:simplehttp, "~> 0.4.1"},
       {:mockery, "~> 2.2.0", runtime: false}
