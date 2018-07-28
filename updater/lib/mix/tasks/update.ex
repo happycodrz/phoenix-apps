c756b9eeec626feaf26050c5f982c485091a4d2f
Roman Heinrich <roman.heinrich@gmail.com>
2018-07-28 20:30:53 +0200

    mix task with more information

--- updater/lib/mix/tasks/update.ex
+++ updater/lib/mix/tasks/update.ex
@@ -3,7 +3,10 @@ defmodule Mix.Tasks.Update do
 
   @shortdoc "Runs the Updater.run/0 function"
   def run(_) do
+    IO.puts "**************** Updating local repos..."
     Updater.Sorter.sort
     Updater.run
+    IO.puts "**************** Updating Readme.md..."
+    Updater.ReadmeUpdater.run()
   end
 end
